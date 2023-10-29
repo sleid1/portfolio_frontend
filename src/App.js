20148788931aea3b8ec252f3bde2f55695d5a20b
sleid1 <68285202+sleid1@users.noreply.github.com>
2023-10-30 00:39:15 +0100

    My First Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect } from 'react';
+import { About, Footer, Header, Skills, Testimonial, Work } from './container';
+import { Navbar } from './components';
+import './App.scss';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+   useEffect(() => {
+      // Store the original title
+      const originalTitle = document.title;
+
+      // Function to update the title when the page becomes hidden
+      const handleVisibilityChange = () => {
+         if (document.hidden) {
+            document.title = 'Return Back...';
+         } else {
+            // Reset the title to the original title
+            document.title = originalTitle;
+         }
+      };
+
+      // Add event listener for visibility change
+      document.addEventListener('visibilitychange', handleVisibilityChange);
+
+      // Clean up the event listener when the component unmounts
+      return () => {
+         document.removeEventListener(
+            'visibilitychange',
+            handleVisibilityChange
+         );
+      };
+   }, []);
+
+   return (
+      <div className="app">
+         <Navbar />
+         <Header />
+         <About />
+         <Work />
+         <Skills />
+         <Testimonial />
+         <Footer />
+      </div>
+   );
 }
 
 export default App;
